5a4e7a159df33a8d857d058d215f4cd2a127e991
nou-ali <noura4403@gmail.com>
2021-12-01 00:56:45 +0000

    Executed sequence of asynchronous tasks in a linear manner

--- survey.js
+++ survey.js
@@ -5,32 +5,31 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Lighthouse Labs? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+const profileDetails = {};
 
-  rl.question("What's your name? Nicknames are also acceptable", (answer) => {
-    console.log(`Thank you for your valuable feedback: ${answer}`);
+rl.question("What's your name? Nicknames are also acceptable", (answer) => {
+  profileDetails.name = answer;
 
-    rl.question("What's an activity you like doing?", (answer) => {
-      console.log(`Thank you for your valuable feedback: ${answer}`);
+  rl.question("What's an activity you like doing?", (answer) => {
+    profileDetails.activty = answer;
 
-      rl.question("What do you listen to while doing that?", (answer) => {
-        console.log(`Thank you for your valuable feedback: ${answer}`);
+    rl.question("What do you listen to while doing that?", (answer) => {
+      profileDetails.music = answer;
 
-        rl.question("Which meal is your favourite (eg: dinner, brunch, etc.)", (answer) => {
-          console.log(`Thank you for your valuable feedback: ${answer}`);
+      rl.question("Which meal is your favourite (eg: dinner, brunch, etc.)", (answer) => {
+        profileDetails.mealTime = answer;
 
-          rl.question("What's your favourite thing to eat for that meal?", (answer) => {
-            console.log(`Thank you for your valuable feedback: ${answer}`);
+        rl.question("What's your favourite thing to eat for that meal?", (answer) => {
+          profileDetails.favouriteMeal = answer;
 
-            rl.question("Which sport is your absolute favourite?", (answer) => {
-              console.log(`Thank you for your valuable feedback: ${answer}`);
+          rl.question("Which sport is your absolute favourite?", (answer) => {
+            profileDetails.sport = answer;
 
-              rl.question("What is your superpower? In a few words, tell us what you are amazing at!", (answer) => {
-                console.log(`Thank you for your valuable feedback: ${answer}`);
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at!", (answer) => {
+              profileDetails.superpower = answer;
 
-                rl.close();
-              });
+              console.log(`My name is ${profileDetails.name}. Pre COVID-19 I was really into ${profileDetails.activty} but I can't do that much anymore. Right now I'm listening to ${profileDetails.music} they're amazing. My favourite meal time is ${profileDetails.mealTime} and I love to eat ${profileDetails.favouriteMeal}. When I'm trying to be active I usually ${profileDetails.sport}, it's my favorite sport. If I had to choose a superpower, it would be ${profileDetails.superpower}.`);
+              rl.close();
             });
           });
         });
@@ -38,3 +37,6 @@ rl.question('What do you think of Lighthouse Labs? ', (answer) => {
     });
   });
 });
+
+
+
